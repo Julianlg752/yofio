3560e79f5410dd1b8517aa90c2958b2381434e56
Julian Linares <julis1997lg@gmail.com>
2021-01-10 18:19:13 -0500

    add db validations

--- Connection.go
+++ Connection.go
@@ -3,16 +3,20 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"os"
 
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func connection() (*sql.DB, error) {
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
+    if dbName == "" || dbUser == "" || dbPassword == "" {
+        return nil, errors.New("Invalid DB Settings")
+    }
 	dbinfo := fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s", dbUser, dbPassword, dbName)
 
 	db, db_err := sql.Open("mysql", dbinfo)
