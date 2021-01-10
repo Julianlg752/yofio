bf4b839ecf2168a14a7791c45ac71546226d9485
Julian Linares <julis1997lg@gmail.com>
2021-01-10 15:43:27 -0500

    Add function to save the investment and if was ok or not

--- CreditAssignment.go
+++ CreditAssignment.go
@@ -4,14 +4,24 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"math"
 	"sort"
 )
 
 type Invest struct {
     Investment int32
+    Successful int
 }
 
+type CreditAssign struct {
+    Credito700 int32
+    Credito500 int32
+    credito300 int32
+}
+
+var OK = 1
+var ERROR = 0
 type CreditAssigner interface {
     Assign(int32) (int32, int32, int32, error)
 }
@@ -24,6 +34,7 @@ func (i Invest) Assign() (int32, int32, int32, error) {
 	rotation := 0
 	temp := creditos
 	cont := 0
+    tries := 0
 	for {
 		if inversion == 0 {
 			break
@@ -37,9 +48,13 @@ func (i Invest) Assign() (int32, int32, int32, error) {
 					break
 				}
 				if math.Signbit(float64(inversion)) {
-					inversion = 0
-					total_creditos[-1] = -1
-					break
+                    inversion = inversion + credito
+                    if tries == len(creditos) {
+					    total_creditos[-1] = -1
+					    inversion = 0
+					    break
+                    }
+                    tries++
 				}
 			}
 			var newarray []int
@@ -49,7 +64,6 @@ func (i Invest) Assign() (int32, int32, int32, error) {
 				temp = newarray
 			}
 			if cont == 3 {
-				fmt.Println()
 				sort.Ints(temp)
 				rotation++
 			}
@@ -60,9 +74,24 @@ func (i Invest) Assign() (int32, int32, int32, error) {
 	}
     for k  := range total_creditos {
         if k == -1 {
+            query := fmt.Sprintf("INSERT INTO investment values(%d,%d)",i.Investment, ERROR)
+            SaveInvestment(query)
             return 0, 0, 0, errors.New("Couldn't Proccess the Investment")
         }
     }
+    query := fmt.Sprintf("INSERT INTO investment values(%d,%d)",i.Investment, OK)
+    SaveInvestment(query)
     return int32(total_creditos[700]), int32(total_creditos[500]), int32(total_creditos[300]), nil
 }
 
+func SaveInvestment(query string) {
+    db_connection, db_err := connection()
+    if db_err != nil {
+        log.Println(db_err)
+    }
+    _, result_err := db_connection.Exec(query)
+    if result_err != nil {
+            log.Println("result_err: ", result_err)
+    }
+
+}
