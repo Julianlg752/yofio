3177cb25f096315f073751d2fbe890a49899076f
Julian Linares <julis1997lg@gmail.com>
2021-01-10 15:44:24 -0500

    add handler to show the statistics and add the response of the investment, also handle the error if something was wrong

--- handlers.go
+++ handlers.go
@@ -9,24 +9,42 @@ import (
     "net/http"
 )
 
+func InvestmentStatistics(w http.ResponseWriter, r *http.Request){
+    investment_list, investment_err := GetInvestments()
+    if investment_err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        fmt.Fprintf(w, investment_err.Error())
+        return
+    }
+    statistics := Statistics(investment_list)
+    fmt.Fprintf(w, statistics)
+}
+
 func CreditAssignment(w http.ResponseWriter, r *http.Request){
     body := r.Body
     rbody, bodyErr := ioutil.ReadAll(body)
     if bodyErr != nil {
         log.Println("Body Error")
         message := "{\"Body Error\":\""+bodyErr.Error()+"\"}"
+        w.WriteHeader(http.StatusBadRequest)
         fmt.Fprintf(w, message)
         return
     }
     var invest Invest
     unmarshalErr := json.Unmarshal(rbody, &invest)
     if unmarshalErr != nil {
-        log.Println("Json Error")
+        log.Println("Json Error: ", unmarshalErr)
         message := "{\"Cannot Parse Json\":\""+unmarshalErr.Error()+"\"}"
+        w.WriteHeader(http.StatusInternalServerError)
         fmt.Fprintf(w, message)
         return
     }
-    fmt.Println(invest.Investment)
-    fmt.Println(invest.Assign())
-    fmt.Fprintf(w, "ping")
+    credit_700, credit_500, credit_300, credit_err := invest.Assign()
+    if credit_err != nil {
+        log.Println(credit_err)
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+    response := fmt.Sprintf("{\"credit_type_300\": %d, \"credit_type_500\": %d, \"credit_type_700\": %d}", credit_300, credit_500, credit_700)
+    fmt.Fprintf(w, response)
 }
